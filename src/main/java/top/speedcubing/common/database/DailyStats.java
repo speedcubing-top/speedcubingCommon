302f04b3542003393bc6a22850ba7cffc6284b4c
speedcubing <git@speedcubing.top>
2024-11-20 01:21:36 +0800

    fix daily thing

--- src/main/java/top/speedcubing/common/database/DailyStats.java
+++ src/main/java/top/speedcubing/common/database/DailyStats.java
@@ -5,14 +5,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DailyStats {
-    public static void validate() {
-        String today = getToday();
-        Database.getSystem().executeUpdate("INSERT IGNORE INTO daily (date) VALUES ("+today+")");
-    }
-
     public static void update(String s) {
-        validate();
+        Database.getSystem().startTransaction();
+        Database.getSystem().executeUpdate("INSERT IGNORE INTO daily (date) VALUES (" + getToday() + ")");
         Database.getSystem().executeUpdate("UPDATE daily SET " + s + " WHERE date='" + getToday() + "'");
+        Database.getSystem().commit();
     }
 
     public static String getToday() {
