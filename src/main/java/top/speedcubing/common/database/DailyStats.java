f8e8f2a3108057056578675531d4d605fbcc6969
speedcubing <git@speedcubing.top>
2024-11-20 01:22:34 +0800

    use doTransaction

--- src/main/java/top/speedcubing/common/database/DailyStats.java
+++ src/main/java/top/speedcubing/common/database/DailyStats.java
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter;
 
 public class DailyStats {
     public static void update(String s) {
-        Database.getSystem().startTransaction();
-        Database.getSystem().executeUpdate("INSERT IGNORE INTO daily (date) VALUES (" + getToday() + ")");
-        Database.getSystem().executeUpdate("UPDATE daily SET " + s + " WHERE date='" + getToday() + "'");
-        Database.getSystem().commit();
+        Database.getSystem().doTransaction(() -> {
+            Database.getSystem().executeUpdate("INSERT IGNORE INTO daily (date) VALUES (" + getToday() + ")");
+            Database.getSystem().executeUpdate("UPDATE daily SET " + s + " WHERE date='" + getToday() + "'");
+        });
     }
 
     public static String getToday() {
