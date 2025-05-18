f1ec7a9491065234acddf10695a45d9b6d19fa39
TheSpeedCubing <speedcubing@speedcubing.top>
2025-05-18 21:30:23 +0800

    fix: default playtime should be 0

--- src/main/java/top/speedcubing/common/database/DailyStats.java
+++ src/main/java/top/speedcubing/common/database/DailyStats.java
@@ -4,12 +4,16 @@ package top.speedcubing.common.database;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import top.speedcubing.lib.utils.SQL.SQLConnection;
+import top.speedcubing.lib.utils.SQL.SQLRuntimeException;
 
 public class DailyStats {
     public static void update(String s) {
         try (SQLConnection connection = Database.getSystem()) {
-            connection.executeUpdate("INSERT INTO daily (date, playtime) VALUES (" + getToday() + ", 0) " +
-                    "ON DUPLICATE KEY UPDATE " + s);
+            try {
+                connection.executeUpdate("INSERT INTO daily (date, playtime) VALUES (" + getToday() + ", 0) ");
+            } catch (SQLRuntimeException ignored) {
+            }
+            connection.executeUpdate("UPDATE daily SET " + s + " WHERE date='" + getToday() + "'");
         }
     }
 
